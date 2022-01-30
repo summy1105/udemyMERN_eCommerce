936c05e9add681f6ea3680c1521a6205d5566883
murraySD <dsm851105@gmail.com>
2022-01-30 21:30:07 +0900

    made product screen

---
+++ frontend/Z_MEMO.md
@@ -0,0 +1,8 @@
+# Memo
+
+## React Router
+course used v5
+to install : "npm I react-router-dom@5.2.0 react-router-bootstrap@0.25.0"
+
+How to use React Router v6
+link youtube : https://www.youtube.com/watch?v=k2Zk5cbiZhg&t=1s
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App= ()=> {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome To ProShop</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path='/' component={HomeScreen} exact/>
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
