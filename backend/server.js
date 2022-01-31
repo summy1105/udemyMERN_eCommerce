46ce575840ad634c7aeea909552d03808c45a578
murraySD <dsm851105@gmail.com>
2022-01-31 22:27:37 +0900

    add middleware (not found, error handler)

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,46 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from "./data/products.js"; // have to type ".js" for file
+
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+
+// express는 스프링과 다르게 handler를 등록하는 순서가 중요하다.
+// 모든 url을 처리하는 handler등록전에 parameter가 3개인 핸들러를 등록하면 request filter역할을 한다.
+// 모든 url을 처라한 후 등록되는 handler는 response filter역할이다.
+// parameter가 4개인 핸들러는 error handler이다. => 그렇지만 이 핸들러의 위치에 따라 처리하는 scope(영역)이 다르다.
+// 그리고 다른 handler가 실행되게 하려면 next()를 넣어주어야 한다.
+
+// 미들웨어로 filter역할을 할 메소드를 등록한다.
+// 다른 request를 처리하는 로직전에 지정하면, request를 처리하기 전에 실행된다.
+app.use((req, res, next)=>{
+  console.log(req.originalUrl);
+  next();
+})
+
+
 app.get("/", (req, res) => {
   res.send("App is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
+
+
+
+// 여기 전에 등록되어 있는 handler에서 해당되는 url이 없을 경우 실행 된다.
+app.use(notFound)
+
+
+// error handler를 등록한다.
+// Error-handling middleware always takes four arguments.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
