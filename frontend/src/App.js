75b29405ca5049fb6de9d92ec8bf4bb861b6ce31
murraySD <dsm851105@gmail.com>
2022-02-02 19:10:59 +0900

    f: login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
             <Route path='/' component={HomeScreen} exact/>
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/login' component={LoginScreen}/>
           </Container>
         </main>
         <Footer />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,19 +4,26 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productDetailReducer, productListReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/carReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage
   = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
+const userInfoFromStorage
+  = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
 const initialState = {
-  cart : { cartItems: []}
+  cart : { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage}
 };
 
 const middleware = [thunk];
